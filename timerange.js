d4702202ac1c0c93bcacf6d48a9372a3a7449e04
Akira Laine <akira_laine@outlook.com>
2016-04-27 16:43:09 +1100

    added 'minute' type support and extra date checks

--- timerange.js
+++ timerange.js
@@ -2,26 +2,35 @@ var Timerange = function(){}
 
 Timerange.prototype.timeFrom = function(date, type){
   if(date instanceof Date){
-    switch (type) {
-      case "days":
-        var dayInMS = 1000 * 60 * 60 * 24;
-        var differenceMs = Math.abs(Date.now() - Date.parse(date));
-        this.result = Math.round(differenceMs / dayInMS) - 1;
-        break;
-      case "hours":
-        var hoursFromTargetYear = date.getYear() === new Date().getYear() ? 0 : 365 * 24;
-        var hoursFromTargetMonth = date.getMonth() === new Date().getMonth() ? 0 : (new Date().getMonth() - date.getMonth()) * 31 * 24;
-        var hoursFromTargetDay = date.getDate() === new Date().getDate() ? 0 : (new Date().getDate() - date.getDate()) * 24;
-        var hoursFromTargetHours = date.getHours() === new Date().getHours() ? 0 : new Date().getHours() - date.getHours();
-        console.log(hoursFromTargetYear, hoursFromTargetMonth, hoursFromTargetDay, hoursFromTargetHours)
-        this.result = hoursFromTargetYear + hoursFromTargetMonth + hoursFromTargetDay + hoursFromTargetHours;
-        console.log(this.result)
-        break;
-      default:
-        return this;
-        break;
+    if(date.getDate() <= new Date().getDate() && date.getHours() <= new Date().getHours() && date.getMinutes() <= new Date().getMinutes()){
+      switch (type) {
+        case "days":
+          var dayInMS = 1000 * 60 * 60 * 24;
+          var differenceMs = Math.abs(Date.now() - Date.parse(date));
+          this.result = Math.round(differenceMs / dayInMS) - 1;
+          break;
+        case "hours":
+          var hoursFromTargetYear = date.getYear() === new Date().getYear() ? 0 : 365 * 24;
+          var hoursFromTargetMonth = date.getMonth() === new Date().getMonth() ? 0 : (new Date().getMonth() - date.getMonth()) * 31 * 24;
+          var hoursFromTargetDay = date.getDate() === new Date().getDate() ? 0 : (new Date().getDate() - date.getDate()) * 24;
+          var hoursFromTargetHours = date.getHours() === new Date().getHours() ? 0 : new Date().getHours() - date.getHours();
+          this.result = hoursFromTargetYear + hoursFromTargetMonth + hoursFromTargetDay + hoursFromTargetHours;
+          break;
+        case "minutes":
+          var minutesFromTargetYear = date.getYear() === new Date().getYear() ? 0 : 365 * 24 * 60;
+          var minutesFromTargetMonth = date.getMonth() === new Date().getMonth() ? 0 : (new Date().getMonth() - date.getMonth()) * 31 * 24 * 60;
+          var minutesFromTargetDay = date.getDate() === new Date().getDate() ? 0 : (new Date().getDate() - date.getDate()) * 24 * 60;
+          var minutesFromTargetHours = date.getHours() === new Date().getHours() ? 0 : new Date().getHours() - date.getHours() * 60;
+          var minutesFromTargetMinute = date.getMinutes() === new Date().getMinutes() ? 0 : new Date().getMinutes() - date.getMinutes();
+          this.result = minutesFromTargetYear + minutesFromTargetMonth + minutesFromTargetDay + minutesFromTargetHours + minutesFromTargetMinute;
+        default:
+          return this;
+          break;
+      }
+      return this;
+    } else {
+      console.error("Timerange.dateFrom() only takes date objects that are in the past.")
     }
-    return this;
   }
 }
 
