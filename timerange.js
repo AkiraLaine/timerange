aa972012e6ef5126f4951aeb6faeed09e3c87e53
Akira Laine <akira_laine@outlook.com>
2016-04-27 16:28:56 +1100

    added support for 'hour' type

--- timerange.js
+++ timerange.js
@@ -1,10 +1,28 @@
 var Timerange = function(){}
 
-Timerange.prototype.timeFrom = function(date){
-  var dayInMS = 1000 * 60 * 60 * 24;
-  var differenceMs = Math.abs(Date.now() - Date.parse(date));
-  this.result = Math.round(differenceMs / dayInMS) - 1;
-  return this;
+Timerange.prototype.timeFrom = function(date, type){
+  if(date instanceof Date){
+    switch (type) {
+      case "days":
+        var dayInMS = 1000 * 60 * 60 * 24;
+        var differenceMs = Math.abs(Date.now() - Date.parse(date));
+        this.result = Math.round(differenceMs / dayInMS) - 1;
+        break;
+      case "hours":
+        var hoursFromTargetYear = date.getYear() === new Date().getYear() ? 0 : 365 * 24;
+        var hoursFromTargetMonth = date.getMonth() === new Date().getMonth() ? 0 : (new Date().getMonth() - date.getMonth()) * 31 * 24;
+        var hoursFromTargetDay = date.getDate() === new Date().getDate() ? 0 : (new Date().getDate() - date.getDate()) * 24;
+        var hoursFromTargetHours = date.getHours() === new Date().getHours() ? 0 : new Date().getHours() - date.getHours();
+        console.log(hoursFromTargetYear, hoursFromTargetMonth, hoursFromTargetDay, hoursFromTargetHours)
+        this.result = hoursFromTargetYear + hoursFromTargetMonth + hoursFromTargetDay + hoursFromTargetHours;
+        console.log(this.result)
+        break;
+      default:
+        return this;
+        break;
+    }
+    return this;
+  }
 }
 
 Timerange.prototype.display = function(){
